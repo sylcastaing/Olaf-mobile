bdc863e9939b538c34fb24261cfe072eb54f616e
Sylvain Castaing <castaing.sylvain@gmail.com>
2017-02-19 17:08:01 +0100

    reinit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,38 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from 'ionic-native';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
-
+import { ContactPage } from '../pages/contact/contact';
 
 @Component({
-  template: `<ion-nav [root]="rootPage"></ion-nav>`
+  templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
   rootPage = TabsPage;
 
+  pages: Array<{title: string, component: any}>;
+
   constructor(platform: Platform) {
+    this.initializeApp(platform);
+
+    this.pages = [
+      { title: 'Contact', component: ContactPage }
+    ];
+  }
+
+  openPage(page) {
+    this.nav.setRoot(page.component);
+  }
+
+  initializeApp(platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+      Splashscreen.hide();
     });
   }
 }
